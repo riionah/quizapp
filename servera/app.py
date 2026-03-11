b4c851cb10b9aebb53906a7874ea5fc904118756
riionah <rh54044@ubt-uni.net>
2026-03-11 18:32:00 +0100

    fix indentation error

--- servera/app.py
+++ servera/app.py
@@ -13,25 +13,23 @@ def create_app():
     jwt.init_app(app)
 
     # Import routes
-from servera.routes.authroutes import auth_bp
-from servera.routes.qiuzroutes import quiz_bp
-from servera.routes.admin import admin_bp
+    from servera.routes.authroutes import auth_bp
+    from servera.routes.qiuzroutes import quiz_bp
+    from servera.routes.admin import admin_bp
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(quiz_bp, url_prefix="/api/quiz")
     app.register_blueprint(admin_bp, url_prefix="/api/admin")
 
-    # Database test + table creation
     from sqlalchemy import text
 
     with app.app_context():
         try:
             db.session.execute(text("SELECT 1"))
             db.create_all()
-            print("✅ Database Connected Successfully")
-            print("✅ Tables Created Successfully")
+            print("Database Connected Successfully")
         except Exception as e:
-            print("❌ Database Error:", e)
+            print("Database Error:", e)
 
     return app
 
